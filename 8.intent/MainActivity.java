ad08f4fbd16dbe532e46d02ace93b84740dc2e7b
ayshasahana <116252387+ayshasahana@users.noreply.github.com>
2023-11-29 15:48:11 +0530

    Add files via upload

---
+++ 8.intent/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1 = findViewById(R.id.button1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                change();
+            }
+        });
+    }
+
+    public void change(){
+        Intent i = new Intent(this,MainActivity2.class);
+        i.putExtra("name","AYSHA");
+        i.putExtra("initial","MCA S3");
+        startActivity(i);
+    }
+}
